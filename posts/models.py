05cb35b45b9830665310ac20bc67c18781e0d939
ZackGuo510 <zackguo@foxmail.com>
2018-03-23 16:04:35 +0800

    initial conmmit

---
+++ posts/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    text = models.TextField(max_length=300)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text[:20] + '...'
