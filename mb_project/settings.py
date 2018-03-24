87c2c15e146e381854e2a9ef2476a5b5c7237050
ZackGuo510 <zackguo@foxmail.com>
2018-03-24 12:26:44 +0800

    asserts

--- mb_project/settings.py
+++ mb_project/settings.py
@@ -94,7 +94,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-STATIC_ROOT = "/home/ZackGuo510/messageboard/static"
+STATIC_ROOT = "/home/ZackGuo510/messageboard/asserts"
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
